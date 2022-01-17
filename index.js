764e2e021e473e1dbfb232721afb3be333dda783
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2022-01-17 16:30:08 +0200

    clock

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ setInterval(printTime, 1000)
 function printTime(){
     const d = new Date();
     const h = d.getHours();
-    const m = (d.getMinutes() < 10)? '0' + d.getMinutes() : d.getMinutes();
-    const s = (d.getSeconds() < 10)? '0' + d.getSeconds() : d.getSeconds();
+    const m = (d.getMinutes() < 10) ? `0${d.getMinutes()}` : d.getMinutes();
+    const s = (d.getSeconds() < 10) ? `0${d.getSeconds()}` : d.getSeconds();
     const h1 = document.createElement('h1');
     const time = document.createTextNode(`${h} : ${m} : ${s}`);
     h1.append(time);
