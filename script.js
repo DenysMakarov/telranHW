ee34a1fd82fbb4d54ea91414b3d1520076f9e6d2
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2022-01-23 18:53:33 +0200

    cmpl

--- script.js
+++ script.js
@@ -1,51 +1,108 @@
-const persons = [];
 
-addPerson.onclick = function () {
-    clearStat();
-    const person = new Person(personId.value.trim(), firstName.value.trim(), lastName.value.trim(), age.value);
-    if (persons.findIndex(p => p.id === person.id) >= 0) {
-        alert(`Person with id = ${person.id} exists`);
-    } else {
-        persons.push(person);
-        const li = createInfoElement(person.toString(), 'li');
-        li.setAttribute('name', person.id);
-        const btn = createInfoElement('click', 'button')
-       
-        btn.onclick = (e) => {
-            clearStat();
-            const el = e.target.parentElement.getAttribute('name');
-            e.target.parentElement.remove()        
-
-            for(let i = 0; i < persons.length; i++) {
-                if(el == persons[i].id) {
-                    persons.splice(i, 1)                    
-                    return;
-                }
-            }
-        }
-        li.append(btn)
-        personsList.append(li);
+class Person {
+    constructor(id, firstName, lastName, birthDate) {
+        this._id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = new Date(birthDate);
     }
-    personId.value = firstName.value = lastName.value = age.value = '';
+
+    fullName() {
+        return `${this.firstName}  ${this.lastName}`
+    }
 }
 
-function clearStat(){
-    const box = document.getElementById('boxStat')
-    if(box != undefined){
-        box.remove()
+class Employee extends Person {
+    constructor(id, firstName, lastName, birthDate, salary) {
+        super(id, firstName, lastName, birthDate);
+        this._salary = +salary;
     }
+
+    get salary() { return this._salary }
+    set salary(salary) {
+        if (salary > this._salary) {
+            this._salary = +salary;
+        }
+    }
+
+    toString = function () {
+        return `ID: ${this._id}, 
+         fullName : ${this.fullName()}, 
+         birth-date: ${this.birthDate.getFullYear()}-${this.birthDate.getDate()}-${this.birthDate.getDay()}, 
+         salary: ${this.salary}`;
+    }
+}
+
+class Company {
+    constructor() {
+        this._employees = []
+        this._stat = {}
+    }
+
+    get employees() {
+        return [...this._employees]; 
+    }
+
+    addEmployee(employee) {
+        if (this._employees.findIndex(e => employee._id === e._id) < 0){
+            this._employees.push(employee)
+            return true
+        } 
+        return false;
+    }
+
+    removeEmployee(id) {
+        this._employees = this._employees.filter(el => el._id !== id);
+    }
+
+    get quantity() { return this._employees.length }
+
+    getStatisctic() {
+        this._stat.salary = Math.round(this._employees.reduce((acc, p) => acc + p.salary, 0) / this._employees.length);
+        let today = new Date().getFullYear();
+        this._stat.middleAge = Math.round(this._employees.reduce((acc, p) => acc + today - p.birthDate.getFullYear(), 0) / this._employees.length)
+        this._stat.min = this._employees.reduce((acc, p) => acc > today - p.birthDate.getFullYear() ? today - p.birthDate.getFullYear() : acc, today - this._employees[0].birthDate.getFullYear(), 0)
+        this._stat.max = this._employees.reduce((acc, p) => acc < today - p.birthDate.getFullYear() ? today - p.birthDate.getFullYear() : acc, today - this._employees[0].birthDate.getFullYear(), 0)
+        return this._stat.max
+    }
+
+}
+
+const firm = new Company();
+
+addPerson.onclick = function () {
+    const person = new Employee(personId.value.trim(), firstName.value.trim(), lastName.value.trim(), birthDate.value, salary.value.trim());
+    if(!firm.addEmployee(person)) return alert(`Person with id = ${person._id} exists`);;
+           
+    clearStats();
+    const li = createInfoElement(person.toString(), 'li');
+    const buttonDel = createInfoElement('X', 'button');
+    buttonDel.setAttribute('name', person._id)
+    buttonDel.classList.add('del');
+
+    buttonDel.onclick = function ({ target }) {
+        target.parentElement.remove();
+        clearStats();
+        firm.removeEmployee(target.getAttribute('name'))
+    }
+
+    li.append(buttonDel);
+    personsList.append(li);
+    personId.value = firstName.value = lastName.value = birthDate.value = '';
 }
 
 calcStats.onclick = function () {
+    if(!firm.employees.length) return alert(`Nothing to stat`);
+    firm.getStatisctic();
+    console.log(firm._stat);
+    
     const divStats = document.createElement('div');
-    divStats.setAttribute('id', 'boxStat')
-    let age = persons.reduce((acc, p) => acc + p.age, 0) / persons.length;
-    const h3avg = createInfoElement(`Average age: ${age.toFixed(1)}`, 'h3');
-    age = persons.reduce((acc, p) => acc > p.age ? p.age : acc, persons[0].age);
-    const h3min = createInfoElement(`Min age: ${age}`, 'h3');
-    age = persons.reduce((acc, p) => acc < p.age ? p.age : acc, persons[0].age);
-    const h3max = createInfoElement(`Max age: ${age}`, 'h3');
-    divStats.append(h3avg, h3min, h3max);
+    const h3avg = createInfoElement(`Average age: ${firm._stat.middleAge}`, 'h3');
+    const h3min = createInfoElement(`Min age: ${firm._stat.min}`, 'h3');
+    const h3max = createInfoElement(`Max age: ${firm._stat.max}`, 'h3');
+    const h3salary = createInfoElement(`Avg salary: ${firm._stat.salary}`, 'h3');
+
+    divStats.append(h3avg, h3min, h3max, h3salary);
     if (stats.firstElementChild.nextElementSibling) {
         stats.replaceChild(divStats, stats.firstElementChild.nextElementSibling);
     } else {
@@ -53,19 +110,15 @@ calcStats.onclick = function () {
     }
 };
 
+function clearStats() {
+    if (stats.firstElementChild.nextElementSibling) {
+        stats.removeChild(stats.firstElementChild.nextElementSibling);
+    }
+}
+
 function createInfoElement(content, tag) {
     const element = document.createElement(tag);
     const text = document.createTextNode(content);
     element.append(text);
     return element;
-}
-
-function Person(id, firstName, lastName, age) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = +age;
-    this.toString = function () {
-        return `ID: ${this.id}, ${this.firstName}, ${this.lastName}, age: ${this.age}`;
-    }
 }
